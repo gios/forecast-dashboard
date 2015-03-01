df7a074f2581409d285d52b4f117c038765d9df5
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2015-03-01 14:20:50 +0200

    Fix port name

--- server.js
+++ server.js
@@ -42,6 +42,4 @@ function* getData(data, next) {
 // Routes
 app.use(route.get('/forecast/:coords', getData));
 
-app.listen(3000);
-
-console.log('listening on port 3000');
+app.listen(process.env.PORT || 3000);
