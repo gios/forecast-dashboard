7bcbf5e230d4d0f376a239febe4dfc6deb05b253
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2015-03-01 14:27:43 +0200

    Uncomment server.js

--- server.js
+++ server.js
@@ -14,32 +14,32 @@ app.use(logger());
 app.use(bodyParser());
 app.use(serve('dist'));
 
-//// Forecast API
-//var forecast = new Forecast({
-//    service: 'forecast.io',
-//    key: '75e082171b2e3f48839528d4feba6de9',
-//    units: 'celcius',
-//    cache: true,
-//    ttl: {
-//        minutes: 27,
-//        seconds: 45
-//    }
-//});
-//
-//// Functions
-//function getForecastData(coords) {
-//    return function(callback) {
-//        forecast.get(coords, callback);
-//    };
-//}
-//
-//function* getData(data, next) {
-//    this.set('Access-Control-Allow-Origin', '*');
-//    var parseData = data.split(",");
-//    this.body = yield getForecastData(parseData);
-//}
-//
-//// Routes
-//app.use(route.get('/forecast/:coords', getData));
+// Forecast API
+var forecast = new Forecast({
+    service: 'forecast.io',
+    key: '75e082171b2e3f48839528d4feba6de9',
+    units: 'celcius',
+    cache: true,
+    ttl: {
+        minutes: 27,
+        seconds: 45
+    }
+});
+
+// Functions
+function getForecastData(coords) {
+    return function(callback) {
+        forecast.get(coords, callback);
+    };
+}
+
+function* getData(data, next) {
+    this.set('Access-Control-Allow-Origin', '*');
+    var parseData = data.split(",");
+    this.body = yield getForecastData(parseData);
+}
+
+// Routes
+app.use(route.get('/forecast/:coords', getData));
 
 app.listen(process.env.PORT || 3000);
